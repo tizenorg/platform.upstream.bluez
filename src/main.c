e8c4b5b5670b50465ff2d495bd9f2b8c21de4ba1
h.sandeep <h.sandeep@samsung.com>
2015-09-30 16:40:44 +0530

    LESC: Add enableSecureConnection option in main.conf
    Change-Id: Ibd4972f6e52dd1eb2c3339e890e3fe003e97ab2d
    Signed-off-by: h.sandeep <h.sandeep@samsung.com>

--- src/main.c
+++ src/main.c
@@ -342,6 +342,17 @@ static void parse_config(GKeyFile *config)
 		g_clear_error(&err);
 	else
 		main_opts.le_privacy = boolean;
+
+	val = g_key_file_get_integer(config, "General",
+						"EnableSecureConnection", &err);
+	if (err) {
+		DBG("%s", err->message);
+		g_clear_error(&err);
+	} else {
+		DBG("EnableSecureConnection=%d", val);
+		main_opts.secure_connection = val;
+	}
+
 #endif
 }
 
